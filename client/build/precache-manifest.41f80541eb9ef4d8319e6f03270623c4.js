de3ee89e64913507cb5a39499156eb46c661ec23
preetama <preetam.satapathy007@gmail.com>
2021-11-27 22:48:13 +0530

    build

---
+++ client/build/precache-manifest.41f80541eb9ef4d8319e6f03270623c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e31f11f0b6f9c8b71102f67a0427db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8ef09025ada7e8a0eed",
+    "url": "/static/css/2.3513b61d.chunk.css"
+  },
+  {
+    "revision": "c242d08bc51a20bd37b8",
+    "url": "/static/css/main.f732e055.chunk.css"
+  },
+  {
+    "revision": "c8ef09025ada7e8a0eed",
+    "url": "/static/js/2.1a3c4498.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a3c4498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c242d08bc51a20bd37b8",
+    "url": "/static/js/main.7d9b4926.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
